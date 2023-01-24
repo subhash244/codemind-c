e18c0039ac9d5c93ab2c2e17b8ecebea58d5ff9b
subhash244 <123383349+subhash244@users.noreply.github.com>
2023-01-24 12:09:17 +0530

    #include<stdio.h> int main() { int n; scanf("%d",&n); int i,j; if(n>=3 && n<=100) { for(i=1;i<=n;i++) { for(j=1;j<=i;j++) { printf("*"); } printf(" "); } for(i=n-1;i>=1;i--) { for(j=i;j>=1;j--) { printf("*"); } printf(" "); } } else printf("The pattern is not possible");}

---
+++ Half_diamond_star_pattern.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main()
+{ int n; scanf("%d",&n); int i,j; if(n>=3 && n<=100) { for(i=1;i<=n;i++) { for(j=1;j<=i;j++) { printf("*"); } printf("
+"); } for(i=n-1;i>=1;i--) { for(j=i;j>=1;j--) { printf("*"); } printf("
+"); } } else printf("The pattern is not possible");}
