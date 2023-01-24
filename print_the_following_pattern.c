ba8f1b4efb4ac50ad54875622ca504ac4d5d1ee1
subhash244 <123383349+subhash244@users.noreply.github.com>
2023-01-24 12:12:55 +0530

    #include<stdio.h> int main() { int n; scanf("%d",&n); int i,j; for(i=1;i<=n;i++) { for(j=1;j<=n;j++) { if(j==i) { printf("0"); } else printf("x"); } printf(" "); }}

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 int main()
-{ int n; scanf("%d",&n); int i,j; for(i=1;i<=n;i++) { for(j=1;j<=i;j++) { if(j==1||j==i||i==n ) printf("*"); else printf(" "); } printf("
+{ int n; scanf("%d",&n); int i,j; for(i=1;i<=n;i++) { for(j=1;j<=n;j++) { if(j==i) { printf("0"); } else printf("x"); } printf("
 "); }}
